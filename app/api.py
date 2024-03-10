233781d0ada6231d00a605ebb4c0e357aaaf5595
gsarthakdev <dev@sarthak.pro>
2024-03-10 03:24:06 -0400

    Add new import

--- app/api.py
+++ app/api.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from werkzeug.utils import secure_filename
+from feat.detector import Detector
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 app = FastAPI()
 
